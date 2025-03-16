e19b6696240b46382f818e942c768ed0ee80dcdc
Chiekko Red Alino <chiekkored@gmail.com>
2025-03-17 07:00:57 +0800

    Add Google Maps and Geocoding support; update Android and iOS configurations

--- lib/presentation/controllers/asset/asset.controller.dart
+++ lib/presentation/controllers/asset/asset.controller.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:lend/core/models/asset.model.dart';
 import 'package:lend/core/models/user.model.dart';
 import 'package:lend/utilities/constants/collections.constant.dart';
@@ -11,23 +13,72 @@ class AssetController extends GetxController {
   Asset? asset = Get.arguments['asset'];
 
   final RxBool _isUserLoading = false.obs;
-  final Rx<User?> _user = Rx(null);
+  final Rxn<User> _user = Rxn();
+
+  final _mapController = Rxn<GoogleMapController>();
+  final markers = <Circle>{}.obs;
+  final RxString _address = ''.obs;
 
   bool get isUserLoading => _isUserLoading.value;
   User? get user => _user.value;
 
+  GoogleMapController? get mapController => _mapController.value;
+  CameraPosition get cameraPosition => CameraPosition(
+      target: LatLng(
+          asset?.location?.latitude ?? 0.0, asset?.location?.longitude ?? 0.0),
+      zoom: 13);
+  String get address => _address.value;
+
   @override
-  void onReady() {
+  void onInit() {
     _getUser();
 
-    super.onReady();
+    super.onInit();
   }
 
   @override
   void onClose() {
+    markers.close();
+
+    _mapController.close();
     _isUserLoading.close();
+    _address.close();
+    _user.close();
 
     super.onClose();
+  }
+
+  void onMapCreated(GoogleMapController mapCtrl) {
+    _mapController.value = mapCtrl;
+
+    markers.add(
+      Circle(
+        circleId: const CircleId('current_location_circle'),
+        center: cameraPosition.target,
+        radius: 500, // Radius in meters
+        fillColor: Colors.blue.withOpacity(0.5),
+        strokeColor: Colors.blue,
+        strokeWidth: 1,
+      ),
+    );
+
+    _getAddressFromLatLng(cameraPosition.target);
+  }
+
+  Future<void> _getAddressFromLatLng(LatLng position) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+      if (placemarks.isNotEmpty) {
+        Placemark place = placemarks.first;
+        _address.value =
+            "${place.street}, ${place.locality}, ${place.isoCountryCode}";
+      } else {
+        _address.value = "No address found";
+      }
+    } catch (e) {
+      _address.value = "Error: $e";
+    }
   }
 
   void _getUser() async {
@@ -39,7 +90,6 @@ class AssetController extends GetxController {
     final result = await usersCollection.doc(asset?.ownerId ?? '').get();
 
     if (result.data() != null) {
-      debugPrint('result.data(): ${result.data()}');
       _user.value = User.fromMap(result.data()!);
       _isUserLoading.value = false;
     }
--- lib/presentation/pages/asset/asset.page.dart
+++ lib/presentation/pages/asset/asset.page.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:lend/presentation/common/buttons.common.dart';
 import 'package:lend/presentation/common/images.common.dart';
 import 'package:lend/presentation/common/spinner.common.dart';
 import 'package:lend/presentation/common/texts.common.dart';
 import 'package:lend/presentation/controllers/asset/asset.controller.dart';
 import 'package:lend/utilities/constants/colors.constant.dart';
+import 'package:lend/utilities/extensions/int.extension.dart';
 
 class AssetPage extends GetView<AssetController> {
   static const routeName = '/asset';
@@ -16,7 +18,36 @@ class AssetPage extends GetView<AssetController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: LNDColors.white,
+      backgroundColor: LNDColors.outline,
+      bottomNavigationBar: Container(
+        height: 105.0,
+        decoration: const BoxDecoration(
+          color: LNDColors.white,
+          border: Border(
+            top: BorderSide(color: LNDColors.hint, width: 0.5),
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LNDText.bold(
+                  text:
+                      'P${controller.asset?.rates?.daily.toMoney() ?? ''} daily',
+                  fontSize: 18.0,
+                ),
+                LNDButton.primary(
+                  text: 'tegxt',
+                  enabled: true,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -88,11 +119,11 @@ class AssetPage extends GetView<AssetController> {
             ),
           ),
           SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 16.0,
-              ),
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              margin: const EdgeInsets.only(bottom: 4.0),
+              color: LNDColors.white,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -104,36 +135,31 @@ class AssetPage extends GetView<AssetController> {
                     text: controller.asset?.category ?? '',
                     color: LNDColors.hint,
                   ),
-                ],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 16.0,
-                right: 16.0,
-              ),
-              child: Row(
-                children: [
-                  const FaIcon(
-                    FontAwesomeIcons.solidStar,
-                    color: LNDColors.primary,
-                    size: 20.0,
-                  ),
                   const SizedBox(
-                    width: 6.0,
+                    height: 16.0,
                   ),
-                  LNDText.bold(
-                    text: '4.8',
-                    color: LNDColors.primary,
-                  ),
-                  const SizedBox(
-                    width: 6.0,
-                  ),
-                  LNDText.regular(
-                    text: '(400 reviews)',
-                    color: LNDColors.hint,
+                  Row(
+                    children: [
+                      const FaIcon(
+                        FontAwesomeIcons.solidStar,
+                        color: LNDColors.primary,
+                        size: 20.0,
+                      ),
+                      const SizedBox(
+                        width: 6.0,
+                      ),
+                      LNDText.bold(
+                        text: '4.8',
+                        color: LNDColors.primary,
+                      ),
+                      const SizedBox(
+                        width: 6.0,
+                      ),
+                      LNDText.regular(
+                        text: '(400 reviews)',
+                        color: LNDColors.hint,
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -142,10 +168,8 @@ class AssetPage extends GetView<AssetController> {
           SliverToBoxAdapter(
             child: Container(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              decoration: const BoxDecoration(
-                color: LNDColors.outline,
-              ),
+              margin: const EdgeInsets.symmetric(vertical: 4.0),
+              color: LNDColors.white,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -154,7 +178,7 @@ class AssetPage extends GetView<AssetController> {
                         left: 16.0, right: 16.0, bottom: 16.0),
                     child: LNDText.medium(
                       text: 'Product Showcase',
-                      fontSize: 16.0,
+                      color: LNDColors.hint,
                     ),
                   ),
                   SizedBox(
@@ -176,65 +200,128 @@ class AssetPage extends GetView<AssetController> {
                       },
                     ),
                   ),
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Divider(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: LNDText.regular(
+                      text: controller.asset?.description ?? '',
+                      overflow: TextOverflow.visible,
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
           SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Obx(
-                () => controller.isUserLoading
-                    ? const LNDSpinner(
-                        color: LNDColors.black,
-                      )
-                    : Row(
-                        children: [
-                          LNDImage.circle(
-                            imageUrl: controller.user?.photoUrl,
-                            size: 40.0,
-                          ),
-                          const SizedBox(
-                            width: 8.0,
-                          ),
-                          LNDText.bold(text: controller.user?.name ?? ''),
-                          const SizedBox(
-                            width: 2.0,
-                          ),
-                          const Icon(
-                            Icons.verified_rounded,
-                            size: 15.0,
-                            color: LNDColors.primary,
-                          )
-                        ],
-                      ),
+            child: Container(
+              margin: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                  color: LNDColors.primary.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(16.0)),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    LNDText.bold(
+                      text: 'Inclusions',
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
           SliverToBoxAdapter(
-            child: Padding(
+            child: Container(
               padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              margin: const EdgeInsets.symmetric(vertical: 4.0),
+              color: LNDColors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      const FaIcon(
-                        FontAwesomeIcons.locationDot,
-                        color: LNDColors.hint,
-                        size: 20.0,
-                      ),
-                      const SizedBox(
-                        width: 8.0,
-                      ),
-                      LNDText.regular(text: 'Location here')
-                    ],
+                  Obx(
+                    () => controller.isUserLoading
+                        ? const LNDSpinner(
+                            color: LNDColors.black,
+                          )
+                        : Row(
+                            children: [
+                              LNDImage.circle(
+                                imageUrl: controller.user?.photoUrl,
+                                size: 40.0,
+                              ),
+                              const SizedBox(
+                                width: 8.0,
+                              ),
+                              LNDText.bold(text: controller.user?.name ?? ''),
+                              const SizedBox(
+                                width: 2.0,
+                              ),
+                              const Icon(
+                                Icons.verified_rounded,
+                                size: 15.0,
+                                color: LNDColors.primary,
+                              )
+                            ],
+                          ),
                   ),
-                  LNDButton.icon(
-                    icon: FontAwesomeIcons.angleRight,
-                    onPressed: () {},
-                    size: 20.0,
-                  )
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const FaIcon(
+                                  FontAwesomeIcons.locationDot,
+                                  color: LNDColors.hint,
+                                  size: 20.0,
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                Obx(
+                                  () =>
+                                      LNDText.regular(text: controller.address),
+                                ),
+                              ],
+                            ),
+                            LNDButton.icon(
+                              icon: FontAwesomeIcons.angleRight,
+                              onPressed: () {},
+                              size: 20.0,
+                            )
+                          ],
+                        ),
+                        Container(
+                          height: 100.0,
+                          margin: const EdgeInsets.symmetric(vertical: 16.0),
+                          width: double.infinity,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16.0),
+                            child: Obx(
+                              () => GoogleMap(
+                                buildingsEnabled: false,
+                                initialCameraPosition:
+                                    controller.cameraPosition,
+                                onMapCreated: controller.onMapCreated,
+                                circles: controller.markers.toSet(),
+                                myLocationButtonEnabled: false,
+                                zoomGesturesEnabled: false,
+                                scrollGesturesEnabled: false,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
